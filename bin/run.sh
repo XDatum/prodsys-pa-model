0848dddc3057cdc00d22a6ba91aa312552e32288
Mikhail Titov <mikhail.a.titov@gmail.com>
2017-09-18 18:30:51 +0300

    fixed env variables definition

--- bin/run.sh
+++ bin/run.sh
@@ -2,16 +2,16 @@
 
 handlername=$1
 
-SERVICE_EXEC_USER='matitov'
-ANALYTICS_CLUSTER='analytix.cern.ch'
+SERVICE_EXEC_USER=matitov
+ANALYTICS_CLUSTER=analytix.cern.ch
 
-CRONJOBS_BASE_DIR='/home/cronjobs'
-SERVICE_KEYTAB='${CRONJOBS_BASE_DIR}/.globus/${SERVICE_EXEC_USER}.keytab'
-#SERVICE_BASE_DIR='${CRONJOBS_BASE_DIR}/services/prodsys-pa-model'
+CRONJOBS_BASE_DIR=/home/cronjobs
+SERVICE_KEYTAB=${CRONJOBS_BASE_DIR}/.globus/${SERVICE_EXEC_USER}.keytab
+#SERVICE_BASE_DIR=${CRONJOBS_BASE_DIR}/services/prodsys-pa-model
 
-P2PA_EXEC_DIR='/afs/cern.ch/user/${SERVICE_EXEC_USER:0:1}/${SERVICE_EXEC_USER}/prodsys-pa-model'
-P2PA_EXEC_MODULE='${P2PA_EXEC_DIR}/bin/${handlername}.py ${@:2}'
-P2PA_REMOTE_ENV='${P2PA_EXEC_DIR}/bin/remote_env.sh'
+P2PA_EXEC_DIR=/afs/cern.ch/user/${SERVICE_EXEC_USER:0:1}/${SERVICE_EXEC_USER}/prodsys-pa-model
+P2PA_EXEC_MODULE=${P2PA_EXEC_DIR}/bin/${handlername}.py ${@:2}
+P2PA_REMOTE_ENV=${P2PA_EXEC_DIR}/bin/remote_env.sh
 
 kinit -k -t ${SERVICE_KEYTAB} ${SERVICE_EXEC_USER}@CERN.CH
 #krenew -K 5 -b
