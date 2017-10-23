f05bda9dcd426d3a49aadfb9d0de1baafa0878ce
mtitov <mikhail.a.titov@gmail.com>
2017-10-24 00:01:01 +0300

    updated launch scripts according new changes (using distributor-module)

--- scripts/generate_predictions_cold.sh
+++ scripts/generate_predictions_cold.sh
@@ -3,6 +3,7 @@
 MODEL_WORK_DIR=model_cold
 NEW_TASKS_WORK_DIR=new_tasks_cold
 QUERY_CONFIG=cfg_new_tasks_cold
+DISTR_METHOD=set_ttcj_timestamp
 
 LOG_FILE=${P2PA_CRONJOBS_LOG_DIR}/generate_predictions_cold.log
 
@@ -10,5 +11,6 @@ echo "$(date +"%Y-%m-%d %H:%M:%S") - starting" > ${LOG_FILE} 2>&1
 
 ${P2PA_MODEL_DIR}/bin/run.sh collector --verbose --force --work-dir ${NEW_TASKS_WORK_DIR} --config ${QUERY_CONFIG} --days 1 --output-type input >> ${LOG_FILE} 2>&1
 ${P2PA_MODEL_DIR}/bin/run.sh predictor --verbose --force --work-dir ${MODEL_WORK_DIR} --data-dir ${NEW_TASKS_WORK_DIR} >> ${LOG_FILE} 2>&1
+${P2PA_MODEL_DIR}/bin/run.sh distributor --verbose --data-dir ${NEW_TASKS_WORK_DIR} --method ${DISTR_METHOD} >> ${LOG_FILE} 2>&1
 
 echo "$(date +"%Y-%m-%d %H:%M:%S") - finished" >> ${LOG_FILE} 2>&1
--- scripts/get_threshold_set.sh
+++ scripts/get_threshold_set.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+WORK_DIR=threshold_set
+QUERY_CONFIG=cfg_threshold_set
+DISTR_METHOD=set_ttcr_dict
+
 LOG_FILE=${P2PA_CRONJOBS_LOG_DIR}/get_threshold_set.log
 
 echo "$(date +"%Y-%m-%d %H:%M:%S") - starting" > ${LOG_FILE} 2>&1
 
-${P2PA_MODEL_DIR}/bin/run.sh collector --verbose --force --work-dir threshold_set --config cfg_threshold_set --days 180 --output-type output >> ${LOG_FILE} 2>&1
+${P2PA_MODEL_DIR}/bin/run.sh collector --verbose --force --work-dir ${WORK_DIR} --config ${QUERY_CONFIG} --days 180 --output-type output >> ${LOG_FILE} 2>&1
+${P2PA_MODEL_DIR}/bin/run.sh distributor --verbose --data-dir ${WORK_DIR} --method ${DISTR_METHOD} >> ${LOG_FILE} 2>&1
 
 echo "$(date +"%Y-%m-%d %H:%M:%S") - finished" >> ${LOG_FILE} 2>&1
