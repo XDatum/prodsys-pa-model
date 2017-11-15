ef1adac289f79e15742a4b080e502c164f5e8caf
Mikhail Titov <mikhail.a.titov@gmail.com>
2017-11-15 12:32:33 +0300

    refactoring creds-env

--- local/bin/run.sh
+++ local/bin/run.sh
@@ -11,7 +11,7 @@ SERVICE_KEYTAB=${P2PA_CRONJOBS_DIR}/.globus/${SERVICE_EXEC_USER}.keytab
 
 P2PA_EXEC_DIR=/afs/cern.ch/user/${SERVICE_EXEC_USER:0:1}/${SERVICE_EXEC_USER}/prodsys-pa-model
 P2PA_EXEC_MODULE="$P2PA_EXEC_DIR/bin/$handlername.py ${@:2}"
-P2PA_REMOTE_ENV=${P2PA_EXEC_DIR}/bin/remote_env.sh
+P2PA_CREDS_ENV=${P2PA_EXEC_DIR}/private/creds_env.sh
 
 kinit -k -t ${SERVICE_KEYTAB} ${SERVICE_EXEC_USER}@CERN.CH
 aklog
@@ -21,5 +21,5 @@ ssh -o UserKnownHostsFile=/dev/null \
     -o GSSAPIAuthentication=yes \
     -o GSSAPIDelegateCredentials=yes \
     ${SERVICE_EXEC_USER}@${ANALYTICS_CLUSTER} \
-    "source $P2PA_REMOTE_ENV; export PYTHONPATH=$PYTHONPATH:$P2PA_EXEC_DIR; $P2PA_EXEC_MODULE"
+    "source $P2PA_CREDS_ENV; export PYTHONPATH=$PYTHONPATH:$P2PA_EXEC_DIR; $P2PA_EXEC_MODULE"
 kdestroy
